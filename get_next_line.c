18d62fdce45b15eddcfaf198ec69e1b801dc28cc
Dimitri LAVAURY-COLLOT <dlavaury@e3r11p10.42.fr>
2017-11-18 20:02:02 +0100

    piste de depart

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,43 @@
 /*   By: dlavaury <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 14:32:58 by dlavaury          #+#    #+#             */
-/*   Updated: 2017/11/16 14:49:30 by dlavaury         ###   ########.fr       */
+/*   Updated: 2017/11/18 20:01:00 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static int		ft_string_traitement(t_fd *file)
+{
+	size_t len;
+
+	len = 0;
+	file->idx = -1;
+	while (++file->idx < file->ret/* && file->buff[file->idx] != '\n'*/)
+	{
+		if (file->buff[file->idx] == '\n')
+		{
+			if (!file->line)
+				if (!(file->temp = ft_strsub(file->buff, 0, file->idx)))
+					return (0);
+		}
+	}
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	if (fd == -1)
+	static t_fd		file[FD_SIZE_MAX];
+
+	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
+	file[fd].read = 1;
+	while (file[fd].read == 1)
+	{
+		ft_bzero(file[fd].buff, BUFF_SIZE + 1);
+		if ((file[fd].ret = read(fd, file[fd].buff, BUFF_SIZE)) == -1)
+			return (-1);
+		if (!ft_string_traitement(&file[fd]))
+			return (-1);;
+	}
+	return (file[fd].read);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,46 @@
 /*   By: dlavaury <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 14:33:19 by dlavaury          #+#    #+#             */
-/*   Updated: 2017/11/16 14:49:32 by dlavaury         ###   ########.fr       */
+/*   Updated: 2017/11/18 20:00:57 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 /*
 **	BIBLIOTHEQUE
 */
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include "libft.h"
+
+/*
+**	MACCROS
+*/
+
+# define BUFF_SIZE 20
+# define FD_SIZE_MAX 1024
+
+/*
+**	STRUCTURES
+*/
+
+typedef struct	s_fd
+{
+	int		ret;
+	int		read;
+	int		idx;
+	char	buff[BUFF_SIZE + 1];
+	char	*temp;
+	char	*line;
+}				t_fd
 
 /*
 **	PROTOTYPES
 */
 
 int	get_next_line(const int fd, char **line);
+
+# endif
